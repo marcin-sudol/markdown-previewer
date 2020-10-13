c37079f78cb38b0affd565aaa62a2b1821327e4e
Marcin <marcin.sudol@outlook.com>
2020-10-13 19:17:26 +0200

    mixins and media queries in scss

--- src/script.js
+++ src/script.js
@@ -6,18 +6,25 @@ const updatePreview = () => {
 
 
 
-// LAYOUT FUNCTIONS
-const setLayoutHorizontal = () => {
-    $("#windows").removeClass("windows-vertical")
-        .addClass("windows-horizontal");
-}
-
-const setLayoutVertical = () => {
+// FUNCTION SETS VERTICAL LAYOUT
+const setVerticalLayout = () => {
     $("#windows").removeClass("windows-horizontal")
         .addClass("windows-vertical");
+
+    $("#btn-v").addClass("btn-active");
+    $("#btn-h").removeClass("btn-active");
 }
 
 
+
+// FUNCTION SETS HORIZONTAL LAYOUT
+const setHorizontalLayout = () => {
+    $("#windows").removeClass("windows-vertical")
+        .addClass("windows-horizontal");
+
+    $("#btn-h").addClass("btn-active");
+    $("#btn-v").removeClass("btn-active");
+}
 
 
 
@@ -25,7 +32,10 @@ $(document).ready(() => {
     // ADD UPDATE ACTION
     $("#editor").on("input", updatePreview);
 
+    // START WITH VERTICAL LAYOUT
+    setVerticalLayout();
+
     // ADD BUTTONS ACTIONS
-    $("#btn-h").click(setLayoutHorizontal);
-    $("#btn-v").click(setLayoutVertical);
+    $("#btn-h").click(setHorizontalLayout);
+    $("#btn-v").click(setVerticalLayout);
 });
