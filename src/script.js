6b87df03422121ef139a774c482a1c6a975eb433
Marcin <marcin.sudol@outlook.com>
2020-10-13 18:49:51 +0200

    buttons change layout

--- src/script.js
+++ src/script.js
@@ -4,6 +4,28 @@ const updatePreview = () => {
     $("#preview").html(marked($("#editor").val()));
 };
 
+
+
+// LAYOUT FUNCTIONS
+const setLayoutHorizontal = () => {
+    $("#windows").removeClass("windows-vertical")
+        .addClass("windows-horizontal");
+}
+
+const setLayoutVertical = () => {
+    $("#windows").removeClass("windows-horizontal")
+        .addClass("windows-vertical");
+}
+
+
+
+
+
 $(document).ready(() => {
-    $("#editor").on('input', updatePreview);
+    // ADD UPDATE ACTION
+    $("#editor").on("input", updatePreview);
+
+    // ADD BUTTONS ACTIONS
+    $("#btn-h").click(setLayoutHorizontal);
+    $("#btn-v").click(setLayoutVertical);
 });
